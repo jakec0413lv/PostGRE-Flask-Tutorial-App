06de984e647f08cc886fb34d0158b8676926a04f
Jacob Cox <62845242+jakec0413lv@users.noreply.github.com>
2021-04-28 12:05:34 -0700

    Add files via upload
    Added emailing capability to email users data from the table

--- flaskappGH.py
+++ flaskappGH.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from send_email import send_email
+from sqlalchemy.sql import func
 
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:-----PW------@localhost/height_collector'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:---password----@localhost/height_collector'
 db=SQLAlchemy(app)
 
 class Data(db.Model):
@@ -30,6 +32,10 @@ def success():
             data=Data(email, height)
             db.session.add(data)
             db.session.commit()
+            average_height=db.session.query(func.avg(Data.height_)).scalar()
+            average_height=round(average_height, 1)
+            count=db.session.query(Data.height_).count()
+            send_email(email, height, average_height, count)
             return render_template("success.html")
         else:
             return render_template("index.html", 
